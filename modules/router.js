558ca9a9a0cbe30ed05d588a038c53f9d8de32ac
Pascal Tassel <tasselp@gmail.com>
2021-11-18 19:28:15 +0100

    add favicon and 404 routes

--- modules/router.js
+++ modules/router.js
@@ -16,8 +16,13 @@ router.get('/settings.json', (_, response) => {
   response.sendFile('settings.json', { root: '.' });
 });
 
+// Favicon
+router.get('/favicon.ico', (_, response) => {
+  response.sendFile('favicon.ico', { root: '.' });
+});
+
 // Assets files
-router.get('/assets/:dir/:file?', (request, response) => {
+router.get('/assets/:dir/:file', (request, response) => {
   const {dir, file} = request.params;
 
   response.sendFile(`assets/${dir}/${file}`, { root: '.' });
@@ -28,5 +33,10 @@ router.get('/data/setDb', dataController.setDb);
 router.post('/data/getRanking', dataController.getRanking);
 router.post('/data/getRank', dataController.getRank);
 router.post('/data/saveScore', dataController.saveScore);
+
+// 404
+router.use((_, response) => {
+  response.status(404).send('404 error, page not found !');
+});
 
 module.exports = router;
