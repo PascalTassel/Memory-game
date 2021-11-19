578e28871dd34a2208e01e08536921cbb0876eec
Pascal Tassel <tasselp@gmail.com>
2021-11-19 18:25:54 +0100

    remove database from settings and connect() method

--- modules/database.js
+++ modules/database.js
@@ -5,11 +5,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host     : process.env.HOST,
   user     : process.env.USER,
-  password : process.env.PASSWORD,
-  database : process.env.DATABASE
+  password : process.env.PASSWORD
 });
-
-// Connect to db
-connection.connect();
 
 module.exports = connection;
