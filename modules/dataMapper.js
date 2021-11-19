4eec2e0952a132879818d05897c87fb96dd21a5d
Pascal Tassel <tasselp@gmail.com>
2021-11-19 16:52:29 +0100

    escaping queries values and identifiers

--- modules/dataMapper.js
+++ modules/dataMapper.js
@@ -9,49 +9,54 @@ const dataMapper = {
    */
   setDatabase: (callback) => {
     connection.query(`
-      CREATE DATABASE IF NOT EXISTS ${connection.config.database}
-    `, callback);
+      CREATE DATABASE IF NOT EXISTS ??
+    `,
+    [connection.config.database], callback);
   },
   /**
    * Create table if not exist
    */
   setTable: (callback) => {
     connection.query(`
-      CREATE TABLE IF NOT EXISTS ${connection.config.database}.${dataMapper.table}( 
+      CREATE TABLE IF NOT EXISTS ??.??( 
         score_id  SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
         player    VARCHAR(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
         score     SMALLINT NOT NULL,
         date      DATETIME NOT NULL
       )
-    `, callback);
+    `,
+    [connection.config.database, dataMapper.table], callback);
   },
   /**
    * Get ranking
    */
   getRanking: (limit, callback) => {
     connection.query(`
-      SELECT player, score, DATE_FORMAT(date, '%Y-%m-%d %H:%i:%s') AS date FROM ${dataMapper.table} 
-      ORDER BY score ASC LIMIT ${limit}
-    `, callback);
+      SELECT player, score, DATE_FORMAT(date, '%Y-%m-%d %H:%i:%s') AS date FROM ?? 
+      ORDER BY score ASC LIMIT ? 
+    `,
+    [dataMapper.table, limit], callback);
   },
   /**
    * Get player rank
    */
   getRank: (score, callback) => {
     connection.query(`
-      SELECT COUNT(score_id) + 1 AS rank FROM ${dataMapper.table} WHERE score <= ${score}
-    `, callback);
+      SELECT COUNT(score_id) + 1 AS rank FROM ?? WHERE score <= ?
+    `,
+    [dataMapper.table, score], callback);
   },
   /**
    * Save player score
    */
   saveScore: (playerData, callback) => {
     connection.query(`
-      INSERT INTO ${dataMapper.table}
+      INSERT INTO ??
         (player, score, date) 
       VALUES 
-        ('${playerData.player}', '${playerData.score}', '${playerData.date}')
-    `, callback);
+        (?, ?, ?)
+    `,
+    [dataMapper.table, playerData.player, playerData.score, playerData.date], callback);
   }
 };
 
